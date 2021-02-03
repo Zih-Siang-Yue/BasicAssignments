bcb338e506c1ddc6cc5eee3f3e31ed44917957d1
Sean Yue <sean.yue@skylinetw.com>
2021-02-03 10:54:15 +0800

    docs: add a .gitignore file

---
+++ CMoneyAssignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CMoneyAssignment2
+//
+//  Created by Sean.Yue on 2021/2/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
